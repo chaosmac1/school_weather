38cd8c09b22fff24c2eada9f5df8bdc2c08dbf33
KamiKuma <90195553+KamiKuma@users.noreply.github.com>
2021-09-20 10:26:41 +0200

    Update client.cs

--- IOTClient/client.cs
+++ IOTClient/client.cs
@@ -11,9 +11,6 @@ char ssid[] dns.getHostByName(hostname, IPv4);
 int status = WL_IDLE_STATUS;
 IPAddress server(ssid);
 
-char json[] = "{\"key\":"[Contence]",\"temp\":"[Contence]",\"windSpeed\":"[Contence]",\"humidity\":"[Contence]",\"windDirection\":"[Contence]"}";
-JsonObject& test_json = jsonBuffer.parseObject(json); //decode/parse the JSON string to a JsonObject
-
 WiFiClient client;
 
 void connect() {
@@ -21,6 +18,19 @@ void connect() {
 	if (client.connect(server, 80)) {
 		Serial.println("connected");
 	}
+}
+
+public static class tool {
+	public static byte[] JsonStringToByteArray(string jsonByteString) {
+        	jsonByteString = jsonByteString.Substring(1, jsonByteString.Length - 2);
+        	string[] arr = jsonByteString.Split(',');
+        	byte[] bResult = new byte[arr.Length];
+        	
+		for (int i = 0; i < arr.Length; i++) {
+        	    bResult[i] = byte.Parse(arr[i]);
+        	}
+        	return bResult;
+    	}
 }
 
 void setup() {
